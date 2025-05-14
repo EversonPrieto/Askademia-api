63e9f4d9db57994bcf64e928b0c41a90f82d14a6
EversonPrieto <eversonlemosprieto@outlook.com>
2025-05-14 14:19:37 -0300

    chore: import e config do cors

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import usuariosRouter from "./routes/usuarios";
+import cors from "cors";
 
 const app = express();
 const PORT = 3004;
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/usuarios', usuariosRouter)
