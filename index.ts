72a287a8a84574137eba109839ce94881ca972a8
EversonPrieto <eversonlemosprieto@outlook.com>
2025-11-13 13:23:31 -0300

    Fix: pfvr vai

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import cors from "cors";
-import adminsRouter from "./routes/admins";
-import usuariosRouter from "./routes/usuarios";
-import perguntasRouter from "./routes/perguntas";
-import respostasRouter from "./routes/respostas";
-import displinasRouter from "./routes/disciplinas";
-import denunciasRouter from "./routes/denuncias";
+import adminsRouter from "./src/routes/admins";
+import usuariosRouter from "./src/routes/usuarios";
+import perguntasRouter from "./src/routes/perguntas";
+import respostasRouter from "./src/routes/respostas";
+import displinasRouter from "./src/routes/disciplinas";
+import denunciasRouter from "./src/routes/denuncias";
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -28,4 +28,6 @@ app.get("/", (req, res) => {
   res.send("API: Sistema Askademia - Q&A");
 });
 
-export default app;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
